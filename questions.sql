47e15078fd5cbf6bd177afc21b21a69e7be3b006
Benson Lau <bensonjaclyn.lau@gmail.com>
2018-04-28 01:28:48 -0700

    first set of questions

--- questions.sql
+++ questions.sql
@@ -1,6 +1,6 @@
 ###Questions from http://www.programmerinterview.com/index.php/database-sql/practice-interview-question-1/
 
-/*
+#See tables
 SELECT
 	*
 FROM programmer_interview.salesperson;
@@ -14,11 +14,11 @@ SELECT
 *
 FROM programmer_interview.orders;
 
-*/
+
 
 #a. The names of all salespeople that have an order with Samsonic.
 SELECT
-	s.name
+	distinct s.name
 FROM
 	orders o 
 LEFT JOIN
@@ -27,8 +27,43 @@ LEFT JOIN
 	Customer c on o.cust_id=c.id
 where c.name='Samsonic'
 ;
+
 #b. The names of all salespeople that do not have any order with Samsonic.
-
+SELECT
+	distinct s.name
+FROM
+	orders o 
+LEFT JOIN
+	salesperson s on o.salesperson_id=s.id
+LEFT JOIN
+	Customer c on o.cust_id=c.id
+where c.name not in ('Samsonic')
+;
+;
 #c. The names of salespeople that have 2 or more orders.
+SELECT
+	s.name,
+	count(*)
+FROM
+	orders o 
+LEFT JOIN
+	salesperson s on o.salesperson_id=s.id
+LEFT JOIN
+	Customer c on o.cust_id=c.id
+GROUP BY 1
+HAVING count(*)>1
+;
 
-#d. Write a SQL statement to insert rows into a table called highAchiever(Name, Age), where a salesperson must have a salary of 100,000 or greater to be included in the table.
+#d. Write a SQL statement to insert rows into a table called highAchiever(Name, Age), 
+#where a salesperson must have a salary of 100,000 or greater to be included in the table.
+DROP TABLE if exists programmer_interview.highAchiever;
+CREATE TABLE programmer_interview.highAchiever
+as
+	SELECT
+		s.name,
+		s.age
+	FROM
+		salesperson s
+	WHERE 
+		s.salary>=100000
+;
