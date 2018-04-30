2f3804daa6329dfcbf4c14ac7fe83e818efa40e2
Benson Lau <bensonjaclyn.lau@gmail.com>
2018-04-30 15:03:45 -0700

    adding in another table 'person'

--- create_initial_tables.sql
+++ create_initial_tables.sql
@@ -61,3 +61,14 @@ VALUES
     ('60','1998-03-2','6','7','720'),
     ('70','1998-05-06','9','7','150')
 ;
+
+CREATE TABLE person (id int, first_name varchar(20), age int, gender char(1));
+
+INSERT INTO person VALUES (1, 'Bob', 25, 'M');
+INSERT INTO person VALUES (2, 'Jane', 20, 'F');
+INSERT INTO person VALUES (3, 'Jack', 30, 'M');
+INSERT INTO person VALUES (4, 'Bill', 32, 'M');
+INSERT INTO person VALUES (5, 'Nick', 22, 'M');
+INSERT INTO person VALUES (6, 'Kathy', 18, 'F');
+INSERT INTO person VALUES (7, 'Steve', 36, 'M');
+INSERT INTO person VALUES (8, 'Anne', 25, 'F');
